173eaa8440ae177712554f55c6c33b8ef5f715be
jessybriard <jessy.briard@hotmail.com>
2021-06-30 16:28:01 +0200

    Part 1 done

--- java/src/main/java/com/google/Video.java
+++ java/src/main/java/com/google/Video.java
@@ -16,18 +16,41 @@ class Video {
     this.tags = Collections.unmodifiableList(tags);
   }
 
-  /** Returns the title of the video. */
-  String getTitle() {
+  /**
+   * Returns the title of the video.
+   * @return The title of the video
+   */
+  public String getTitle() {
     return title;
   }
 
-  /** Returns the video id of the video. */
-  String getVideoId() {
+  /**
+   * Returns the video id of the video.
+   * @return The video id of the video
+   * */
+  public String getVideoId() {
     return videoId;
   }
 
-  /** Returns a readonly collection of the tags of the video. */
-  List<String> getTags() {
+  /**
+   * Returns a readonly collection of the tags of the video.
+   * @return A collection of the tags of the video
+   */
+  public List<String> getTags() {
     return tags;
   }
+
+  /**
+   * Returns the full display String of the video, under the "title (video_id) [tags]" format.
+   * @return The full display String of the video
+   */
+  public String getFullDisplayString() {
+    String displayString = title + " (" + videoId + ") [";
+    for (String tag: tags) {
+      displayString = displayString + tag + " ";
+    }
+    displayString = displayString.trim() + "]";
+    return displayString;
+  }
+
 }
--- java/src/main/java/com/google/VideoPlayer.java
+++ java/src/main/java/com/google/VideoPlayer.java
@@ -1,47 +1,160 @@
 package com.google;
 
+import java.util.*;
+
 public class VideoPlayer {
 
   private final VideoLibrary videoLibrary;
+  private final List<Video> videos;
+  private Video currentlyPlayingVideo;
+  private HashMap<String, Video> videosPerId;
+  private boolean videoIsPaused = true;
+  private HashMap<String, VideoPlaylist> playlists;
 
   public VideoPlayer() {
     this.videoLibrary = new VideoLibrary();
+    this.videos = videoLibrary.getVideos();
+    associateVideosWithId();
+    playlists = new HashMap<>();
+  }
+
+  /**
+   * This method instantiates the videosPerId HashMap, which allows a Video object to be retrieved from its videoId.
+   */
+  public void associateVideosWithId() {
+    videosPerId = new HashMap<>();
+    for (Video video: videos) {
+      videosPerId.put(video.getVideoId(), video);
+    }
   }
 
   public void numberOfVideos() {
-    System.out.printf("%s videos in the library%n", videoLibrary.getVideos().size());
+    System.out.printf("%s videos in the library%n", videos.size());
   }
 
   public void showAllVideos() {
-    System.out.println("showAllVideos needs implementation");
+
+    //We sort the videos
+    TreeMap<String, Video> sortedVideos = new TreeMap<>();
+    for (Video video: videos) {
+      sortedVideos.put(video.getTitle(), video);
+    }
+
+    //We print the list of available videos
+    System.out.println("Here's a list of all available videos:");
+    for (Video video: sortedVideos.values()) {
+      System.out.println(video.getFullDisplayString());
+    }
   }
 
   public void playVideo(String videoId) {
-    System.out.println("playVideo needs implementation");
+
+    //We check if there is a video associated with videoId
+    Video chosenVideo = videosPerId.get(videoId);
+    if (chosenVideo == null) {
+      //The video does not exist, we print a warning message and end the method
+      System.out.println("Cannot play video: Video does not exist");
+      return;
+    }
+
+    //We stop the video that is currently playing
+    if (currentlyPlayingVideo != null) {
+      System.out.println("Stopping video: " + currentlyPlayingVideo.getTitle());
+    }
+
+    //We play the chosen video (if the given videoId is valid)
+    currentlyPlayingVideo = chosenVideo;
+    System.out.println("Playing video: " + currentlyPlayingVideo.getTitle());
+    videoIsPaused = false;
+
   }
 
   public void stopVideo() {
-    System.out.println("stopVideo needs implementation");
+
+    if (currentlyPlayingVideo != null) {
+      //There is a video currently playing, we stop it
+      System.out.println("Stopping video: " + currentlyPlayingVideo.getTitle());
+      currentlyPlayingVideo = null;
+    } else {
+      //There is no video currently playing, we display a warning message
+      System.out.println("Cannot stop video: No video is currently playing");
+    }
+
   }
 
   public void playRandomVideo() {
-    System.out.println("playRandomVideo needs implementation");
+
+    int numberOfVideos = videos.size();
+
+    if (numberOfVideos == 0) {
+      //No videos available, we print a warning message and end the method
+      System.out.println("No videos available");
+      return;
+    }
+
+    Random random = new Random();
+    Video randomVideo = videos.get(random.nextInt(numberOfVideos));
+    playVideo(randomVideo.getVideoId());
+
   }
 
   public void pauseVideo() {
-    System.out.println("pauseVideo needs implementation");
+
+    if (currentlyPlayingVideo == null) { //There is no video playing
+      System.out.println("Cannot pause video: No video is currently playing");
+    }
+    else if (videoIsPaused) { //The video is already paused
+      System.out.println("Video already paused: " + currentlyPlayingVideo.getTitle());
+    }
+    else { //The video is not paused, we pause the video
+      System.out.println("Pausing video: " + currentlyPlayingVideo.getTitle());
+      videoIsPaused = true;
+    }
+
+
   }
 
   public void continueVideo() {
-    System.out.println("continueVideo needs implementation");
+
+    if (currentlyPlayingVideo == null) { //There is no video playing
+      System.out.println("Cannot continue video: No video is currently playing");
+    }
+    else if (videoIsPaused) { //The video is already paused, we continue the video
+      System.out.println("Continuing video: " + currentlyPlayingVideo.getTitle());
+      videoIsPaused = false;
+    }
+    else { //The video is not paused
+      System.out.println("Cannot continue video: Video is not paused");
+    }
+
   }
 
   public void showPlaying() {
-    System.out.println("showPlaying needs implementation");
+
+    if (currentlyPlayingVideo == null) { //There is no video playing
+      System.out.println("No video is currently playing");
+    } else { //There is a video playing
+      System.out.print("Currently playing: " + currentlyPlayingVideo.getFullDisplayString());
+      if (videoIsPaused) {
+        System.out.print(" - PAUSED");
+      }
+      System.out.println();
+    }
+
   }
 
   public void createPlaylist(String playlistName) {
-    System.out.println("createPlaylist needs implementation");
+
+    //There is no need to handle playlist names with whitespaces as the CommandParser uses whitespaces to separate command words,
+    //therefore the passed playlistName parameter does not contain a whitespace
+
+    if (playlists.get(playlistName.toLowerCase()) == null) { //There is no playlist with the same name
+      playlists.put(playlistName, new VideoPlaylist());
+      System.out.println("Successfully created new playlist: " + playlistName);
+    } else { //There is already a playlist with the same name
+      System.out.println("Cannot create playlist: A playlist with the same name already exists");
+    }
+
   }
 
   public void addVideoToPlaylist(String playlistName, String videoId) {
